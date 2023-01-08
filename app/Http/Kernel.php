c69f9976277c60d4e51f12b5b3beeaab7d0c629f
Denissant <denissanturyan@gmail.com>
2023-01-09 02:08:25 +0400

    feat: configure sanctum

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,9 @@ class Kernel extends HttpKernel
 		],
 
 		'api' => [
-			// \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+			\Illuminate\View\Middleware\ShareErrorsFromSession::class,
+			\Illuminate\Session\Middleware\StartSession::class,
+			\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 			'throttle:api',
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
 		],
