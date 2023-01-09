57f0bca354adcae2ecd9f20a9a51f18693121dec
Denissant <denissanturyan@gmail.com>
2023-01-09 18:53:15 +0400

    refactor: use custom email verify notification

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\VerifyEmailNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -47,6 +48,11 @@ class User extends Authenticatable implements MustVerifyEmail
 	protected function setPasswordAttribute($password)
 	{
 		$this->attributes['password'] = bcrypt($password);
+	}
+
+	public function sendEmailVerificationNotification()
+	{
+		$this->notify(new VerifyEmailNotification);
 	}
 
 	public function primaryEmail()
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,10 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Notifications\Messages\MailMessage;
-use URL;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,32 +24,5 @@ class AuthServiceProvider extends ServiceProvider
 	public function boot()
 	{
 		$this->registerPolicies();
-
-		VerifyEmail::createUrlUsing(function ($notifiable) {
-			$frontendUrl = config('app.frontend_url');
-
-			$verifyUrl = URL::temporarySignedRoute(
-				'verification.verify',
-				now()->addMinutes(config('auth.verification.expire', 120)),
-				[
-					'id'   => $notifiable->getKey(),
-					'hash' => sha1($notifiable->getEmailForVerification()),
-				]
-			);
-
-			return $frontendUrl . '?verify_url=' . urlencode($verifyUrl);
-		});
-
-		VerifyEmail::toMailUsing(function ($notifiable, $url) {
-			return (new MailMessage)
-				->subject('Please verify your email address')
-				->line('Movie Quotes')
-				->line('Hola ' . $notifiable->username)
-				->line('Thanks for joining Movie quotes! We really appreciate it. Please click the button below to verify your account:')
-				->line('If clicking doesn\'t work, you can try copying and pasting it to your browser:')
-				->line('If you have any problems, please contact us: support@moviequotes.ge')
-				->line('MovieQuotes Crew')
-				->action('Verify account', $url);
-		});
 	}
 }
