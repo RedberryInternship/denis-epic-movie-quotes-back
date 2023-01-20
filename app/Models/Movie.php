d7c14d0c66ec55a56b49aec422bbc2bd258c5b18
Denissant <denissanturyan@gmail.com>
2023-01-20 22:29:44 +0400

    feat: create movies table and model

---
+++ app/Models/Movie.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Movie extends Model
+{
+	use HasFactory;
+
+	use HasTranslations;
+
+	protected $guarded = [];
+
+	public array $translatable = ['title', 'description', 'director'];
+
+	public function user()
+	{
+		return $this->belongsTo(User::class, 'user_id');
+	}
+
+	public function quotes()
+	{
+		return $this->hasMany(Quote::class);
+	}
+}
