f1799f34dd32f3494c59667ee7451483c66fdc6d
Denissant <denissanturyan@gmail.com>
2023-01-20 22:41:17 +0400

    fix: unhandled google login error

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -21,6 +21,7 @@ return [
 	'logout_success'               => 'Successfully logged out',
 	'reset_sent'                   => 'Password reset link sent',
 	'google_failed_already_exists' => 'Failed to Sign in with Google – a user with this email address already exists. Please log in with your Movie Quotes password',
+	'google_failed'                => 'Failed to Sign in with Google. Please try again.',
 	'password_changed'             => 'Password changed successfully',
 	'account_activated'            => 'Your account has been activated.',
 ];
