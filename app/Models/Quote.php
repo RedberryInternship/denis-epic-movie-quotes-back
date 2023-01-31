d9e319e01b163b2f8d74f2df6ca8d6a57b5cef74
Denissant <denissanturyan@gmail.com>
2023-01-31 13:02:38 +0400

    feat: add search query param to quote get request

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -36,8 +36,41 @@ class Quote extends Model
 		return $this->hasMany(Like::class);
 	}
 
-	public function scopeCurrentUserLikes($query)
+	public function scopeSearchByBodyOrMovieTitle($sqlQuery, $searchQuery = null)
 	{
-		$query->with(['likes' => fn ($query) => $query->where('user_id', auth()->user()->id)]);
+		if (!$searchQuery)
+		{
+			return;
+		}
+
+		$cleanSearchQuery = $this->removeCharIfStartsWith($searchQuery, '#');
+		$cleanSearchQuery = $this->removeCharIfStartsWith($cleanSearchQuery, '@');
+
+		if (!str_starts_with($searchQuery, '@'))
+		{
+			$sqlQuery->where('body', 'LIKE', "%$cleanSearchQuery%")
+					 ->orWhere('body->ka', 'LIKE', "%$cleanSearchQuery%");
+		}
+		if (!str_starts_with($searchQuery, '#'))
+		{
+			$sqlQuery->orWhereHas('movie', function ($query) use ($cleanSearchQuery) {
+				$query->where('title', 'LIKE', "%$cleanSearchQuery%")
+					  ->orWhere('title->ka', 'LIKE', "%$cleanSearchQuery%");
+			});
+		}
+	}
+
+	public function scopeCurrentUserLikes($sqlQuery)
+	{
+		$sqlQuery->with(['likes' => fn ($query) => $sqlQuery->where('user_id', auth()->user()->id)]);
+	}
+
+	protected function removeCharIfStartsWith($string, $character)
+	{
+		if (str_starts_with($string, $character))
+		{
+			$string = substr($string, 1);
+		}
+		return $string;
 	}
 }
