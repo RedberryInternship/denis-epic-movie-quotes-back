23f365d323587cd8efda570968aa7f34b0baa74b
Denissant <denissanturyan@gmail.com>
2023-02-07 23:39:35 +0400

    feat: add genre index route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\EmailController;
 use App\Http\Controllers\ForgotPasswordController;
+use App\Http\Controllers\GenreController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\QuoteController;
@@ -35,6 +36,8 @@ Route::middleware('auth:sanctum')->group(function () {
 	Route::get('/logout', [AuthController::class, 'logout']);
 
 	Route::get('/newsfeed-quotes', [QuoteController::class, 'index']);
+
+	Route::get('/genre', [GenreController::class, 'index']);
 	Route::post('/like', [LikeController::class, 'like']);
 	Route::post('/comment', [CommentController::class, 'store']);
 
