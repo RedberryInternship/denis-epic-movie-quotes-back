b17d76a853ee71d2bc7659195ab0ddad6a5a6af4
Denissant <denissanturyan@gmail.com>
2023-02-13 05:51:48 +0400

    feat: add notification event

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\NotificationEvent;
 use App\Http\Requests\LikeRequest;
 use App\Models\Like;
+use App\Models\Notification;
+use App\Models\Quote;
 
 class LikeController extends Controller
 {
@@ -26,6 +29,19 @@ class LikeController extends Controller
 			'user_id' => $userID,
 			'quote_id'=> $args['quote_id'],
 		]);
+
+		$targetQuote = Quote::find($args['quote_id']);
+		if ($targetQuote->user->id !== $userID)
+		{
+			$notification = Notification::create(
+				[
+					'is_comment'   => false,
+					'from_user_id' => $userID,
+					'to_user_id'   => $targetQuote->user->id,
+				]
+			);
+			NotificationEvent::dispatch($notification);
+		}
 
 		return response()->json(['message' => 'Quote liked successfully.']);
 	}
