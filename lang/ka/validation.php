152af55a94501c30f27c759d650b7d98f568eaca
Denissant <denissanturyan@gmail.com>
2023-02-18 15:31:16 +0400

    chore: add translation strings

--- lang/ka/validation.php
+++ lang/ka/validation.php
@@ -1,27 +1,41 @@
 <?php
 
 return [
-	'confirmed'            => 'პაროლები არ ემთხვევა ერთმანეთს.',
-	'current_password'     => 'პაროლი არასწორია.',
-	'email'                => ':attribute არაა ვალიდური.',
-	'required'             => ':attributeს შეყვანა აუცილებელია.',
-	'unique'               => 'ეს :attribute დაკავებულია.',
+	'confirmed'               => 'პაროლები არ ემთხვევა ერთმანეთს.',
+	'current_password'        => 'პაროლი არასწორია.',
+	'email'                   => ':attribute არაა ვალიდური.',
+	'required'                => ':attributeს შეყვანა აუცილებელია.',
+	'unique'                  => 'ეს :attribute დაკავებულია.',
 
-	'max'                  => [
+	'max'                     => [
 		'string'           => ':attribute უნდა შეიცავდეს მაქსიუმუმ :max სიმბოლოს.',
 	],
-	'min'                  => [
+	'min'                     => [
 		'string'           => ':attribute უნდა შეიცავდეს მინიმუმ :min სიმბოლოს.',
 	],
 
-	'verify'               => 'გთხოვთ გაიარეთ ელ.ფოსტის ვერიფიკაცია სანამ შეხვალთ ანგარიშში',
-	'invalid_credentials'  => 'შეყვანილი ელ.ფოსტა ან პაროლი არასწორია',
+	'verify'                  => 'გთხოვთ გაიარეთ ელ.ფოსტის ვერიფიკაცია სანამ შეხვალთ ანგარიშში',
+	'invalid_credentials'     => 'შეყვანილი ელ.ფოსტა ან პაროლი არასწორია',
+	'language'                => 'გთხოვთ ტექსტი შეიყვანოთ ',
+	'in_english'              => 'ინგლისურად',
+	'in_georgian'             => 'ქართულად',
+	'only_lowercase_and_nums' => 'გამოიყენეთ მხოლოდ დაბალი რეგისტრის ასოები და ციფრები',
+	'max_current_year'        => ':attribute არ უნდა იყოს მიმდინარე წელზე მეტი',
 
-	'attributes'           => [
+	'attributes'              => [
 		'email'                 => 'ელ-ფოსტა',
 		'username'              => 'მომხმარებლის სახელი',
 		'password'              => 'პაროლი',
 		'confirm_password'      => 'პაროლი',
 		'password_confirmation' => 'პაროლი',
+		'title_en'              => 'ინგლისურენოვანი სათაური',
+		'title_ka'              => 'ქართულენოვანი სათაური',
+		'director_en'           => 'რეჟისორის სახელი',
+		'director_ka'           => 'რეჟისორის სახელი',
+		'body_en'               => 'ციტატის ტექსტი',
+		'body_ka'               => 'ციტატის ტექსტი',
+		'release_year'          => 'release year',
+		'budget'                => 'ბიუჯეტი',
+		'movie'                 => 'ფილმი',
 	],
 ];
