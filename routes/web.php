f276d077730b410c1a97a85dbe675493863f6a52
Denissant <denissanturyan@gmail.com>
2023-02-27 14:25:35 +0400

    chore: add route import statement to web.php

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@
 */
 
 use App\Http\Controllers\GoogleController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/oauth/redirect', [GoogleController::class, 'redirect']);
 Route::get('/oauth/callback', [GoogleController::class, 'callback']);
